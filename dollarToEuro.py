486a687d1d003eedce829d7118ccfbba89d7942f
js080905@icloud.com <js080905@icloud.com>
2022-03-31 09:58:44 -0500

    s

--- dollarToEuro.py
+++ dollarToEuro.py
@@ -2,9 +2,25 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Dollar to Euro Converter")
+window.resizable(width=False, height=False)
 
-ent_dollars
+frm_entry = tk.Frame(master=window)
+ent_dollars = tk.Entry(master = frm_entry, width = 10)
+lbl_dollars = tk.Label(master = frm_entry, text = "\N(US Dollars)"
 
-lbl_result
+ent_dollars.grid(row=0, column=0, sticky="e")
+lbl_dollars.grid(row=0, column=1, sticky="w")
 
-btn_convert "\N{RIGHTWARDS BLACK ARROW}"
+btn_convert = tk.button(
+    master = window,
+    text =  "\N{RIGHTWARDS BLACK ARROW}"
+)
+lbl_result = tk.Label(master=window, text="\N{Euros}")
+
+frm_entry.grid(row=0, column=0, padx=10)
+btn_convert.grid(row=0, column=1, pady=10)
+lbl_result.grid(row=0, column=2, padx=10)
+
+window.mainloop()
+
+import tkinter as tk
