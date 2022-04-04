6d6ac0166bfc69a49416c326aab54d14c1c3264d
js080905@icloud.com <js080905@icloud.com>
2022-04-04 10:33:37 -0500

    commit.

--- dollarToEuro.py
+++ dollarToEuro.py
@@ -6,16 +6,16 @@ window.resizable(width=False, height=False)
 
 frm_entry = tk.Frame(master=window)
 ent_dollars = tk.Entry(master = frm_entry, width = 10)
-lbl_dollars = tk.Label(master = frm_entry, text = "\N(US Dollars)"
+lbl_dollars = tk.Label(master = frm_entry, text = "US Dollar")
 
 ent_dollars.grid(row=0, column=0, sticky="e")
 lbl_dollars.grid(row=0, column=1, sticky="w")
 
-btn_convert = tk.button(
+btn_convert = tk.Button(
     master = window,
     text =  "\N{RIGHTWARDS BLACK ARROW}"
 )
-lbl_result = tk.Label(master=window, text="\N{Euros}")
+lbl_result = tk.Label(master=window, text="Euros")
 
 frm_entry.grid(row=0, column=0, padx=10)
 btn_convert.grid(row=0, column=1, pady=10)
@@ -23,4 +23,19 @@ lbl_result.grid(row=0, column=2, padx=10)
 
 window.mainloop()
 
+
 import tkinter as tk
+
+def dollar_to_euros():
+    """Convert the users given value for US Dollars to Euros and insert
+    the result into lbl_result.
+    """
+    USD = int(input("How many USD?  "))
+    euros = 1.1045(USD)
+    lbl_result["text"] = f"{round(euros, 3)} euros"
+
+btn_convert = tk.Button(
+master=window,
+text="\N{RIGHTWARDS BLACK ARROW}",
+command = dollar_to_euros
+)
